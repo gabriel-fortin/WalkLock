abf5ae31f035fe93760427dc4611acfc32406887
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-21 12:50:23 +0100

    add and hadle layout

--- app/src/main/java/com/example/g14/walklock/MainActivity.kt
+++ app/src/main/java/com/example/g14/walklock/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.g14.walklock
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val tag = MainActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener(this::start)
+        stopButton.setOnClickListener(this::stop)
     }
 
+    fun start(v: View) {
+        val minutes = Integer.parseInt(minutesEditText.text.toString())
+        val seconds = Integer.parseInt(secondsEditText.text.toString())
+        val duration = 60 * minutes + seconds
+    }
+
+    fun stop(v: View) {
+    }
 }
 
 
