24d39f64d30e1c016e17147635e365e60512aa0d
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2017-06-21 12:48:53 +0100

    new project

---
+++ app/src/main/java/com/example/g14/walklock/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.g14.walklock
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
+
+
